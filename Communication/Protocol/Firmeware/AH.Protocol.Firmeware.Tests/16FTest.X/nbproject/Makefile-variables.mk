9981bfff622c157db775537979786321207527be
Clivedurr <clivedurr@gmail.com>
2016-10-24 10:16:30 -0200

    Add the Protocol Firmeware

---
+++ Communication/Protocol/Firmeware/AH.Protocol.Firmeware.Tests/16FTest.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TestingPic.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TestingPic.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=testingpic.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testingpic.x.tar
